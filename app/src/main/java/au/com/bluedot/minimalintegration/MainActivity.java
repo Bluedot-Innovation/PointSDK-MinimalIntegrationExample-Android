ab842685fb3b58e77eaa49e536cae8d14e3a4847
nehabluedot <neha@bluedot.io>
2020-12-15 11:26:54 +1100

    Minimal Integration App pointed to Bluedot SDK 15.3.0

--- app/src/main/java/au/com/bluedot/minimalintegration/MainActivity.java
+++ app/src/main/java/au/com/bluedot/minimalintegration/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.Toast;
+import au.com.bluedot.point.net.engine.BDError;
 import au.com.bluedot.point.net.engine.ServiceManager;
+import au.com.bluedot.point.net.engine.TempoService;
 
 /*
  * @author Bluedot Innovation
@@ -13,7 +16,14 @@ import au.com.bluedot.point.net.engine.ServiceManager;
  */
 public class MainActivity extends Activity implements View.OnClickListener{
 
-    Button bStopSDK;
+    Button bInit;
+    Button bReset;
+
+    Button bStartGeoTrigger;
+    Button bStopGeoTrigger;
+
+    Button bStartTempo;
+    Button bStopTempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +35,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     private void initUI() {
 
-        bStopSDK = (Button) findViewById(R.id.bStopSDK);
-
-        bStopSDK.setOnClickListener(this);
+        //bStopSDK = (Button) findViewById(R.id.bStopSDK);
+        //
+        //bStopSDK.setOnClickListener(this);
     }
 
     private void stopSDK(){
@@ -35,14 +45,48 @@ public class MainActivity extends Activity implements View.OnClickListener{
         finish();
     }
 
+    private void startSDK(){
+
+
+   }
+
     @Override
     public void onClick(View v) {
         int ID = v.getId();
-        switch (ID){
-            case R.id.bStopSDK:
-                stopSDK();
+        MainApplication mainApplication = (MainApplication) getApplicationContext();
+        switch (ID) {
+            case R.id.bInit:
+                mainApplication.initPointSDK();
                 break;
 
+            case R.id.bReset:
+                mainApplication.reset();
+                break;
+
+            case R.id.bStartGeoT:
+                mainApplication.startGeoTrigger();
+                break;
+
+            case R.id.bStopGeoT:
+                mainApplication.stopGeoTrigger();
+                break;
+
+            case R.id.bStartTempo:
+                mainApplication.startTempo();
+                break;
+
+            case R.id.bStopTempo:
+                BDError bdError = TempoService.stop(getApplicationContext());
+                String text = "Tempo stop";
+                if (bdError != null) {
+                    text = text + bdError.getReason();
+                } else {
+                    text = text + "Success";
+                }
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
         }
     }
 
