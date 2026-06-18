3c06d1d7cd5df056cda95523f02e59f6aab58e70
Neha Ishwar <neha@bluedot.io>
2026-06-18 15:28:53 +1000

    removed unused import

--- app/src/main/java/au/com/bluedot/minimalintegration/MainActivity.java
+++ app/src/main/java/au/com/bluedot/minimalintegration/MainActivity.java
@@ -1,7 +1,6 @@
 package au.com.bluedot.minimalintegration;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
